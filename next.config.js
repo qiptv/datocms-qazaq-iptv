81fdb4437e5f567dacb895ed29c067e3c8beb85f
qiptv <114007354+qiptv@users.noreply.github.com>
2023-05-04 22:04:48 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 module.exports = {
   i18n: {
-    locales: ["en", "it"],
-    defaultLocale: "en",
+    locales: ["kk", "ru"],
+    defaultLocale: "kk",
   },
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
